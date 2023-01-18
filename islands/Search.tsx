d11d3667b3427d121176b7fd1748441cce39ba5e
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-01-18 12:28:52 -0800

    fix: visual issues with search

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -102,7 +102,7 @@ export default function Search() {
           setQuery("");
           setResults([]);
         }}
-        class={`absolute left-0 right-0 top-0 bottom-0 bg-black bg-opacity-20 justify-center items-center ${
+        class={`fixed left-0 right-0 top-0 bottom-0 bg-black bg-opacity-20 justify-center items-center ${
           show ? "flex" : "hidden"
         }`}
       >
